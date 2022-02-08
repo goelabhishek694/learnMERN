70820df18db09d7977ec0041fd542e8304d207b5
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-08 23:02:08 +0530

    js datae types done

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-// staritng JS-> JavaScript
-// var let const
+// // staritng JS-> JavaScript
+// // var let const
 
-// varibale declaration
-var a; //int string float ? 
+// // varibale declaration
+// var a; //int string float ? 
 
-// variable initalization
-a = 10;
-console.log(typeof a);
-console.log(a);
+// // variable initalization
+// a = 10;
+// console.log(typeof a);
+// console.log(a);
 
-a = "hello"; //re initialize of varibale 'a'
-console.log(a);
+// a = "hello"; //re initialize of varibale 'a'
+// console.log(a);
 
-// typeof operator-> it tells us the type of data stored in a variable
-console.log(typeof a);
+// // typeof operator-> it tells us the type of data stored in a variable
+// console.log(typeof a);
 
-a = true;
-console.log(a);
-console.log(typeof a);
+// a = true;
+// console.log(a);
+// console.log(typeof a);
 
-a = null;
-console.log(a);
-console.log(typeof a);
+// a = null;
+// console.log(a);
+// console.log(typeof a);
+
+//Number
+var num = 10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+//Boolean
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//String
+// ' ' , " " , ``
+// single quotes, double quotes, backtick 
+var str = 'a';
+console.log(typeof str);
+str = "How you doin ?"
+console.log(str);
+
+
+
+
 
