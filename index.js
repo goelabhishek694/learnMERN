b960f7948db59e3e0f4c11ba53df993bd8b5efb0
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-08 23:50:59 +0530

    basic js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 
 // // variable initalization
 // a = 10;
+// // variable reinitalization
+// a = 20;
 // console.log(typeof a);
 // console.log(a);
 
@@ -25,38 +27,74 @@
 
 //Number
 var num = 10;
-console.log(num);
+// console.log(num);
 var float = 2.4;
-console.log(float);
+// console.log(float);
 
 //Boolean
 var t = true;
 var f = false;
-console.log(t);
-console.log(f);
+// console.log(t);
+// console.log(f);
 
 //String
 // ' ' , " " , ``
 // single quotes, double quotes, backtick 
 var str = 'a';
-console.log(typeof str);
+// console.log(typeof str);
 str = "How you doin ?\ni am fine. how about you ?";
 //backslash n("\n")
-console.log(str);
+// console.log(str);
 
 var b = `hi hope you guys are 
 able grasp the concepts`;
-console.log(b);
+// console.log(b);
 
 var num = 4500;
 // ${}
-console.log(`half of ${num} is ${num / 2}`);
-
-
+// console.log(`half of ${num} is ${num / 2}`);
 
 
 
 //var has some problem
+
+// 1) redeclaration is allowed 
+var r = "hello";
+console.log(r);
+
+var r = 100;
+console.log(r);
+
+// overcome krne ke liye 
+
+// let keyword is used
+let l = 100;
+console.log(l);
+
+// let l = "200"; //SyntaxError: Identifier 'l' has already been declared
+// console.log(l);
+
+//we can reinitialize a variable but we cannot redeclare it
+l = 300;
+console.log(l);
+
+//loops in JS
+var num = 10;
+
+// for (var i = 0; i < num; i++){
+//     if (i % 2 == 0) {
+//         console.log("num is even");
+//     }
+// }
+
+//const keyword
+const a = 2;
+console.log(a);
+// na hi redeclare na reinitialize
+// const a = 5;
+a = 4; //TypeError: Assignment to constant variable.
+console.log("hey");
+
 
 
 
