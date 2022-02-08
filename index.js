ff32941e58b115d14848adaf917a714a22ff34dc
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-08 22:55:26 +0530

    js started

---
+++ index.js
@@ -0,0 +1,25 @@
+// staritng JS-> JavaScript
+// var let const
+
+// varibale declaration
+var a; //int string float ? 
+
+// variable initalization
+a = 10;
+console.log(typeof a);
+console.log(a);
+
+a = "hello"; //re initialize of varibale 'a'
+console.log(a);
+
+// typeof operator-> it tells us the type of data stored in a variable
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
