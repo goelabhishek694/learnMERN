98490d752564557e7d0ca139010ad0d54a3b3141
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-12 22:54:00 +0530

    functions

--- functions.js
+++ functions.js
@@ -1,3 +1,59 @@
-{
-    let a = 20;
+// 3 types of function
+
+//1) Normal Function
+
+//function definiton
+// function function_name(param1, param2) {
+    //do something
+// }
+
+// function invoke
+// function_name(arg1, arg2);
+
+// function add(a, b) {
+//     console.log( a + b);
+// }
+
+
+// add(2, 6);
+
+// functions are treated as first class citizens in Javascript
+//  -> functions can be returned 
+//  -> functions can be passed as parameters/argument
+
+function calculator(str,a,b) {
+    if (str == 'add') {
+        return function add() {
+            console.log( a + b);
+        }
+    }
+    // else if (str == 'sub') {
+        
+    // }
 }
+let returnedFunc = calculator('add', 2, 3);
+console.log("returned function is\n" + returnedFunc);
+returnedFunc();
+
+
+
+
+// 2) Function Expression
+
+// let variable_name = function () {
+//     // do something
+// }
+// name of variable is ised to invoke the funciton
+// variable_name();
+
+let sayHi=function () {
+    console.log("hello guys is am function expression");
+}
+
+console.log("line 54\n" + sayHi);
+sayHi();
+//abcd(); // error abcd is not defined
+
+
+
+
