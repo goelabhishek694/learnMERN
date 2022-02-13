95102c3b13af6cd8e46048bc24dfe33d8d3e7692
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-13 22:31:10 +0530

    2d arrays completed

--- JS/arrays.js
+++ JS/arrays.js
@@ -52,6 +52,8 @@ let array2d = [
   [7, "8", 9],
 ]; //array of arrays
 
+// contigous allocation of memory 
+
 console.log(array2d); //prints 2d array
 console.table(array2d); // prints 2d array in the from of a table 
 let res = array2d[1]; //[ 4, 5, 6 ] 
@@ -60,6 +62,15 @@ console.log(res);
 console.log("res of 2nd index ");
 console.log(res[2]);
 console.log(array2d[1][2]);
+console.log(array2d[1][3]); //undefined 
+console.log(array2d.length); // number of rows in a 2d array 
+console.log(array2d[0].length) // number of columns in a 2d array
+// memory allocation in 2d 
+// 2d mein value kaise daale
+array2d[1][1] = false;
+console.table(array2d);
+//directory
+
 
 
 
