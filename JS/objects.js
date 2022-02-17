ec6e7d1a12f2e94bea1af21ef6979c2aa63a0228
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-17 22:54:25 +0530

    objects started

--- JS/objects.js
+++ JS/objects.js
@@ -3,8 +3,9 @@
 let obj = {};  //empty object
 console.log(obj);
 
+//person object
 let person = {
-    //key : value 
+    //key(property,method()) : value 
     name: "Abhishek",
     age: 28,
     phone: 9999999999,
@@ -42,15 +43,40 @@ console.log(captainAmerica.friends);
 console.log(captainAmerica.friends[0]);
 captainAmerica.sayHi(); //method accessing 
 
-str{
-    length: // 
-    toUpperCase: function() {
-        
-        for()
-    }
+//for loop 
+// in keyword in JS is used to get keys from that object
+for (let haathi in captainAmerica) {
+    //key
+    console.log(haathi);
+    // hathi jo varibale h uske andar meri key ki value padi hui h 
+    //value
+    // varibale ke andar jo binded value h wo chahiye 
+    console.log(captainAmerica[haathi]); //bracket notation usage
 }
+
+
+
+// str{
+//     length: // 
+//     toUpperCase: function() {
+
+//         for()
+//     }
+// }
+
+
 
 
 
 // methods-> objects ke andar function
 // console.log(str.toUpperCase());
+
+
+let arr = ["a", "b", 1, true];
+
+let arrr = {
+    0: "a",
+    1: "b",
+    2: 1,
+    3:true
+}
