0a2f8244296b253b4e46f8ea9b215ad8363c8484
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-19 23:58:41 +0530

    automatic Type converison completed

---
+++ JS/automaticTypeConversion.js
@@ -0,0 +1,22 @@
+var ans = "5" + 1;
+           // "5"+"1" -> 51
+var ans = 1 + "5";
+      // "1"+"5" -> 15
+console.log(ans);
+
+console.log(null * 5); //null is converted to 0 hence output is 0
+console.log(null * "5"); // 0 anything multiplied by zero is obiously 0  
+console.log(undefined * 5); //Nan 
+
+console.log("5" - 1);
+            //5-1 -> 4
+
+console.log("ten" * 3); //"ten" string cannot be mapped to a number like "5" can be mapped to Number 5 hence the ouput is Nan
+console.log("10" * 3);
+
+
+            
+
+
+
+
