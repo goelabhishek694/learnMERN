7a8cb69cc2d2517121ad1833acaac7ec348f3a8d
goelabhishek694 <goelabhishek694@gmail.com>
2022-02-22 23:59:43 +0530

    scopes def added

--- JS/scopes.js
+++ JS/scopes.js
@@ -7,3 +7,13 @@ function outer() {
 var b = 10;
 outer();
 console.log(b);
+
+
+/*scope:
+where can this variable/function be accesed in the code 
+                            OR 
+is variable/function inside the scope of code/function*/
+
+
+// lexical env = local memory + lexical env of parent;
+// scope chain: chain of lexical environment and the parent refferences.
