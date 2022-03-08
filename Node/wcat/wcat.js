24ddb0355c46c4fdcb0dc74ea8c7cf3685404300
goelabhishek694 <goelabhishek694@gmail.com>
2022-03-08 23:45:48 +0530

    wcat 2 commands completed

--- Node/wcat/wcat.js
+++ Node/wcat/wcat.js
@@ -1,31 +1,46 @@
 // 1) node wcat.js filepath => displays the contents of a file in terminal
 // 2) node wcat.js filepath1 filepath2 filepath3  => displays the content of all files in terminal in concatinated form in given order
+// 3) node wcat.js -n file1 file 2 file3 OR node wcat.js -n file1 
 //node wcat.js f1.txt
 //node wcat.js f1.txt f2.txt f3.txt
 const fs = require("fs");
-let inputArr = process.argv.slice(2);
+// let input = process.argv;
 // console.log(input);
-
+let inputArr = process.argv.slice(2);
+console.log(inputArr);
 let filesArr = [];
-// placed files path in filesArr
+let optionsArr = [];
+//===============> placed files path in filesArr <=============
 for (let i = 0; i < inputArr.length; i++){
-    filesArr.push(inputArr[i]);
+    let firstChar = inputArr[i].charAt(0);
+    // console.log(firstChar);
+    if (firstChar == '-') {
+        optionsArr.push(inputArr[i]);
+    }
+    else {
+        filesArr.push(inputArr[i]);
+    }
 }
 // console.log("file to be read are " + filesArr);
 
-//check if all the files are present //
+//=============>check if all the files are present<============= //
 for (let i = 0; i < filesArr.length; i++){
     let doesExist = fs.existsSync(filesArr[i]);
     if (!doesExist) {
-        console.log("Files does not exist ");
+        console.log("One or more File(s) do not exist ");
         return;
+        // break;
     }
 }
 
-// content read and appending starts//
+// =============>content read and appending starts<=============//
 let content = "";
 for (let i = 0; i < filesArr.length; i++){
     let fileContent = fs.readFileSync(filesArr[i]);
-    content += fileContent+"\n";
+    content = content + fileContent + "\n";
+                     
 }
 console.log(content);
+
+let contentArr = content.split("\n");
+console.log(contentArr);
