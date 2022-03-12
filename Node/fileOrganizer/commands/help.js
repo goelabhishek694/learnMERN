b21658b1c0c6c30a2c4b0c24a2cf7c626eca271c
goelabhishek694 <goelabhishek694@gmail.com>
2022-03-12 22:10:51 +0530

    help command done

---
+++ Node/fileOrganizer/commands/help.js
@@ -0,0 +1,20 @@
+function help() {
+    console.log(`
+        These are some myCLI commands used in various situations:
+
+            1. node main.js tree <path>
+            2. node main.js organize <path>
+            3. node main.js help
+    `);
+}
+
+// function abc() {
+//     console.log("in help.js");
+// }
+
+module.exports = {
+   //key value
+    help: help,
+    // haathi:help
+    // ghoda:abc
+}
--- Node/fileOrganizer/main.js
+++ Node/fileOrganizer/main.js
@@ -1,16 +1,22 @@
 // entry point of my command line 
-
+let helpFunc = require("./commands/help");
+// console.log(helpFunc.ghoda());
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
-switch (command) {
+let path = inputArr[1];
+switch (command) { //organizee
     case "tree":
         //call tree function
+        console.log("tree function called and executed succesfully on path " + path);
         break;
     case "organize":
         //call organize function
+        console.log("organize function called and executed succesfully on path "+ path);
         break;
     case "help":
         //call help function
+        helpFunc.help();
+        // console.log("help function called and executed succesfully");
         break;
     default:
         console.log("command not recognized :/")
