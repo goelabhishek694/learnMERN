0a202ce743debaa477e8c5132132e48000682233
goelabhishek694 <goelabhishek694@gmail.com>
2022-03-29 23:44:32 +0530

    :fire: automation added

---
+++ Node/automation/hackerrank/hackerRank.js
@@ -0,0 +1,13 @@
+const puppeteer = require("puppeteer");
+
+let browserOPenPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+  executablePath: "/path/to/Chrome",
+});
+browserOPenPromise
+    .then(function (browser) {
+    // console.log("browser is open");
+        
+})
