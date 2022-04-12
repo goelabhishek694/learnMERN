ef1951cc8c4d671e4b2fb6823e699b9948b1b57f
goelabhishek694 <goelabhishek694@gmail.com>
2022-04-12 22:44:58 +0530

    :zap:async code working

---
+++ Node/4.automation/poc/setTimeout.js
@@ -0,0 +1,53 @@
+{
+function yolo() {
+    var a = 10;
+    function cb(err, res) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("hello how are you ?");
+            
+        }
+    }
+    // function to be executed(callback fun) , time(ms) after which cb to be executed
+    // 1 second =1000 milli second
+    setTimeout(cb, 5000);
+    console.log(a);
+
+}
+
+yolo();
+let b = 100;
+console.log(b);
+}
+
+
+
+const request = require("request");
+let url ="https://www.worldometers.info/coronavirus/"
+function yolo() {
+    var a = 10;
+    function cb(err, res,body) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("hello how are you ?");
+            console.log("statusCode is ",res.statusCode);
+        }
+    }
+    // function to be executed(callback fun) , time(ms) after which cb to be executed
+    // 1 second =1000 milli second
+    request(url,cb)
+    console.log(a);
+
+}
+
+yolo();
+let b = 100;
+console.log(b);
+
+
+
+
