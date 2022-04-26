81788bf69e4bf4d4fb2113a139b33f2e4929e805
goelabhishek694 <goelabhishek694@gmail.com>
2022-04-26 22:03:15 +0530

    :fire:cb, promises interview ques completed

--- Node/4.automation/poc/callback_Interview.js
+++ Node/4.automation/poc/callback_Interview.js
@@ -2,31 +2,31 @@
 
 const fs = require("fs");
 
-//callback hell
-// fs.readFile("f1.txt", function cb1(err, res) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(res + "");
-//     fs.readFile("f2.txt", function cb2(err, res) {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(res + "");
-//         fs.readFile("f3.txt", function cb3(err, res) {
-//           if (err) {
-//             console.log(err);
-//           } else {
-//             console.log(res + "");
-//             console.log("read all files");
-//           }
-//         })
-//       }
-//     });
-//   }
-// });
+//callback hell-> pyramid of doom 
+fs.readFile("f1.txt", function cb1(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res + "");  
+    fs.readFile("f2.txt", function cb2(err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res + "");
+        fs.readFile("f3.txt", function cb3(err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(res + "");
+            console.log("read all files");
+          }
+        })
+      }
+    });
+  }
+});
 
-// 1st way to overcome callback hell is to separate the callabck functions 
+// 1st way to overcome callback hell is to separate the callback functions 
 
 fs.readFile("f1.txt", cb1);
 
--- Node/4.automation/poc/promises_Interview.js
+++ Node/4.automation/poc/promises_Interview.js
@@ -4,29 +4,47 @@
 
 const fs = require("fs");
 
-let f1readPromise = fs.promises.readFile("f1.txt");
+// let f1readPromise = fs.promises.readFile("f1.txt");
 
-function readf2file(f1kadata) {
-  console.log(f1kadata + "");
-  let f2readPromise = fs.promises.readFile("f2.txt");
-  return f2readPromise;
+// function readf2file(f1kadata) {
+//   console.log(f1kadata + "");
+//   let f2readPromise = fs.promises.readFile("f2.txt");
+//   return f2readPromise;
+// }
+
+// function readf3File(f2kadata) {
+//   console.log(f2kadata + "");
+//   let f3readPromise = fs.promises.readFile("f3.txt");
+//   return f3readPromise;
+// }
+
+// function alldone(f3kadata) {
+//   console.log(f3kadata + "");
+//   console.log("read all files serially ");
+// }
+
+// f1readPromise
+//   .then(readf2file)
+//   .then(readf3File)
+//   .then(alldone)
+//   .catch(function (err) {
+//       console.log(err)
+//   });
+
+
+//q2. read 3 files f1.txt,f2.txt,f3.txt paralelly using promises
+
+let f1p = fs.promises.readFile("f1.txt");
+let f2p = fs.promises.readFile("f2.txt");
+let f3p = fs.promises.readFile("f3.txt");
+
+f1p.then(function cb(data) {
+  console.log(data + "");
+});
+
+f2p.then(cb);
+f3p.then(cb);
+
+function cb(data) {
+  console.log(data + "");
 }
-
-function readf3File(f2kadata) {
-  console.log(f2kadata + "");
-  let f3readPromise = fs.promises.readFile("f3.txt");
-  return f3readPromise;
-}
-
-function alldone(f3kadata) {
-  console.log(f3kadata + "");
-  console.log("read all files serially ");
-}
-
-f1readPromise
-  .then(readf2file)
-  .then(readf3File)
-  .then(alldone)
-    .catch(function (err) {
-        console.log(err)
-    });
