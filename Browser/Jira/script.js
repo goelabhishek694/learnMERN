14f1a4677ea007c16e591a65535989756a7e6c8d
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-17 23:47:13 +0530

    :hammer: updates script

--- Browser/Jira/script.js
+++ Browser/Jira/script.js
@@ -7,9 +7,8 @@ let modalPriorityColor = colors[colors.length - 1]; //black
 let textAreaCont = document.querySelector(".textarea-cont");
 const mainCont = document.querySelector(".main-cont");
 let ticketsArr = [];
-
 let toolBoxColors = document.querySelectorAll(".color");
-
+let removeBtn = document.querySelector(".remove-btn");
 //to open close modal container
 let isModalPresent = false;
 addBtn.addEventListener("click", function () {
@@ -65,6 +64,9 @@ function createTicket(ticketColor, data, ticketId) {
 
     mainCont.appendChild(ticketCont);
 
+    handleRemoval(ticketCont, id);
+    handleColor(ticketCont, id);
+    handleLock(ticketCont, id);
     //if ticket is being created for the first time , then ticketId would be undefined
     if (!ticketId) {
         ticketsArr.push(
@@ -92,7 +94,8 @@ for (let i = 0; i < toolBoxColors.length; i++){
         let currToolBoxColor = toolBoxColors[i].classList[0];
 
         let filteredTickets = ticketsArr.filter(function (ticketObj) {
-            return currToolBoxColor == ticketObj.ticketColor;
+            if (currToolBoxColor == ticketObj.ticketColor) return ticketObj;
+            // return currToolBoxColor == ticketObj.ticketColor;
         });
 
         //remove all the tickets
@@ -113,7 +116,8 @@ for (let i = 0; i < toolBoxColors.length; i++){
 
     //to display all the tickets of all colors on double clicking
     toolBoxColors[i].addEventListener("dblclick", function () {
-      //remove all the color specific tickets 
+      
+        //remove all the color specific tickets
         let allTickets = document.querySelectorAll(".ticket-cont");
       for (let i = 0; i < allTickets.length; i++) {
         allTickets[i].remove();
@@ -126,4 +130,71 @@ for (let i = 0; i < toolBoxColors.length; i++){
     })
 }
 
+//on clicking removeBtn, make color red and amke color white in clicking again
+let removeBtnActive = false;
+removeBtn.addEventListener("click", function () {
+    if (removeBtnActive) {
+        removeBtn.style.color = "white";
+    }
+    else {
+        removeBtn.style.color = "red";
+    }
+    removeBtnActive = !removeBtnActive;
+});
+
+// removes ticket from local storage and UI
+function handleRemoval(ticket, id) {
+    ticket.addEventListener("click", function () {
+        if (!removeBtnActive) return;
+        //local storage remove 
+        //->get idx of the ticket to be deleted
+        let idx = getTicketIdx(id);
+        ticketsArr.splice(idx, 1);
+        
+        //removed from browser storage and set updated arr 
+        localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+
+        //frontend remove
+        ticket.remove();
+    });
+}
+
+//returns index of the ticket inside Local Storage's array
+function getTicketIdx(id) {
+    let ticketIdx=ticketsArr.findIndex(function (ticketObj) {
+        return ticketObj.ticketId == id;
+    })
+    return ticketIdx;
+}
+
+//change priority color of the tickets
+function handleColor(ticket, id) {
+    let ticketColorStrip = ticket.querySelector(".ticket-color"); 
+
+    ticketColorStrip.addEventListener("click", function () {
+        let currTicketColor = ticketColorStrip.classList[1]; //lightpink
+        //["lightpink", "lightgreen", "lightblue", "black"];
+        let currTicketColorIdx = colors.indexOf(currTicketColor); //0
+
+        let newTicketColorIdx = currTicketColorIdx + 1; //1
+
+        newTicketColorIdx = newTicketColorIdx % colors.length; //1
+        let newTicketColor = colors[newTicketColorIdx]; //lightgreen
+
+        ticketColorStrip.classList.remove(currTicketColor); //lightpink [ticket-color, lightpink]-> [ticket-color]
+        ticketColorStrip.classList.add(newTicketColor);
+
+        //local storage update 
+        let ticketIdx = getTicketIdx(id);
+        ticketsArr[ticketIdx].ticketColor = newTicketColor;
+        localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+
+    });
+}
+
+//lock and unlock to make content editable true or false 
+function handleLock(ticket, id) {
+    //icons ko append in ticket
+    //toggle of icons and contenteditable property
+}
 
