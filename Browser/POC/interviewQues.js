fd91d869371c44b39585145d4674c2c561ffd390
goelabhishek694 <goelabhishek694@gmail.com>
2022-06-05 03:05:42 +0530

    :zap: destructuring

--- Browser/POC/interviewQues.js
+++ Browser/POC/interviewQues.js
@@ -181,3 +181,116 @@ obj.square();
 
 
 
+let fruits = ["apple", "pear"];
+fruits[5] = "watermelon";
+console.log(fruits[3]);
+fruits.age = 34;
+console.log(fruits.length);
+fruits[7] = "fre";
+console.log(fruits.length);
+fruits.year = "2022";
+console.log(fruits.length);
+fruits.month = "2022";
+fruits[2] = "banana";
+console.log(fruits);
+fruits.abc = "xyz";
+console.log(fruits);
+fruits.length = 4;
+
+// destructuring
+
+let name = ["fjp5", "dev101", "pep"];
+let [batchName, subj] = name;
+console.log(batchName);
+let [, , place] = name;
+
+
+
+let user = {
+  name: "John",
+  age: 30,
+};
+
+// loop over keys-and-values
+for (let details of Object.entries(user)) {
+  console.log(details[0]); // name:John,
+  console.log(details[1]); //age:30
+}
+
+for (let [key,value] of Object.entries(user)) {
+  console.log(key); // name:John,
+  console.log(value); //age:30
+}
+
+// interview ques
+// swap 2 numbers without using temp variable
+let num1 = 10;
+let num2 = 20;
+
+[num2, num1] = [num1, num2]
+console.log(num1);
+console.log(num2);
+// [20.10]
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+
+// let name = ['Hi', [ 'ki','llo'],'yu']
+// let [a,b,c] = name;
+// in this example i'm getting 
+// a = 'H'
+// b = 'i'
+// c = ','
+
+//defsult values
+let family = ["mummy"];
+let [mom, dad="papa"] = family;
+console.log(mom);
+console.log(dad);
+
+function add(x, y=0) {
+  return x + y;
+}
+
+let ans = add(4, 5);
+let a = add(6);
+
+let options = {
+  title: "Menu",
+  width: 100,
+  height: 200,
+};
+//variable names should match that of object , order of writing those variables do not matter 
+// let { title, width, height } = options;
+let { width, height, title } = options;
+console.log(title);
+console.log(width);
+console.log(height);
+
+// let { width: sonu, height: monu, title: raju } = options;
+console.log(sonu);
+console.log(monu);
+console.log(raju);
+
+let { width=200, height: monu, title: raju , name="Ajvinder"} = options;
+
+
+
+let {title, ...rest} = options;
+
+// now title="Menu", rest={height: 200, width: 100}
+console.log(rest.height);  // 200
+console.log(rest.width);   //100
+
+if() { 
+  jrbverfcwed
+  redws
+  refdws
+}
+
+let title, width, height; //->same as let title; let height; let width;
+
+// error in this line
+({ title, width, height } = { title: "Menu", width: 200, height: 100 });
