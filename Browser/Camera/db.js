dbcf41e833516361a60e80df2f88ff40c03b3c51
goelabhishek694 <goelabhishek694@gmail.com>
2022-06-14 22:56:07 +0530

    :chart_with_upwards_trend: indexedDB

---
+++ Browser/Camera/db.js
@@ -0,0 +1,20 @@
+//connect to database
+let db;
+let openRequest = indexedDB.open('myDatabase');
+
+openRequest.addEventListener('success', () => {
+    console.log('db connected');
+    db = openRequest.result;
+})
+
+openRequest.addEventListener("upgradeneeded", () => {
+    console.log("db upgraded OR initalised db ");
+    db = openRequest.result;
+
+    db.createObjectStore('video', { keyPath: "id" });
+    db.createObjectStore("image", { keyPath: "id" });
+});
+
+openRequest.addEventListener("error", () => {
+    console.log("db error");
+});
