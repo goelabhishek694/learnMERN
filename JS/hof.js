86c67e23e7456e8cb9c3a506810ec5aec411fce4
goelabhishek694 <goelabhishek694@gmail.com>
2022-06-21 22:48:56 +0530

    :fire: map filter reduce

--- JS/hof.js
+++ JS/hof.js
@@ -1,15 +1,14 @@
-
 //function calculator is a hof
 function calculator(operator, a, b) {
-    if (operator == "+") {
-        return function add() {
-          return a + b;
-        };
-    }
+  if (operator == "+") {
+    return function add() {
+      return a + b;
+    };
+  }
 }
 console.log("hello");
 let fn = calculator("+", 3, 4);
-let ans = fn();
+var ans = fn();
 console.log(ans);
 
 //functional programming
@@ -18,22 +17,22 @@ const arr = [2, 5, 8, 6, 78];
 
 // calculate the area of squares
 let areaOfSquare = function (arr) {
-    let res = [];
-    for (let i = 0; i < arr.length; i++){
-        res.push(arr[i] * arr[i]);
-    }
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i] * arr[i]);
+  }
 
-    return res;
-}
+  return res;
+};
 
 console.log(areaOfSquare(arr));
 
-//calculate the perimeter of square 
+//calculate the perimeter of square
 
 let perimeterOfSquare = function (arr) {
   let res = [];
   for (let i = 0; i < arr.length; i++) {
-    res.push(4* arr[i]);
+    res.push(4 * arr[i]);
   }
 
   return res;
@@ -46,26 +45,25 @@ console.log(perimeterOfSquare(arr));
 let diagonalOfSquare = function (arr) {
   let res = [];
   for (let i = 0; i < arr.length; i++) {
-      res.push(Math.sqrt(2) * arr[i]);
+    res.push(Math.sqrt(2) * arr[i]);
   }
   return res;
 };
 
 console.log(diagonalOfSquare(arr));
 
-
-//better way 
+//better way
 let area = function (a) {
-    return a * a;
-}
+  return a * a;
+};
 
 let perimeter = function (a) {
-    return 4 * a;
-}
+  return 4 * a;
+};
 
 let diagonal = function (a) {
-    return Math.sqrt(2) * a;
-}
+  return Math.sqrt(2) * a;
+};
 
 let calculate = function (arr, logic) {
   let res = [];
@@ -80,12 +78,11 @@ console.log(calculate(arr, area));
 console.log(calculate(arr, perimeter));
 console.log(calculate(arr, diagonal));
 
-
-//map 
+//map
 //write a code to calculate area of square
 
 let res = arr.map((a) => {
-    return a * a;
+  return a * a;
 });
 
 console.log(res);
@@ -94,21 +91,104 @@ console.log(res);
 // write a code to return an array of even numbers
 
 let evenNum = arr.filter((num) => {
-    return !(num % 2);
+  return !(num % 2);
 });
 console.log(evenNum);
 
-//reduce 
+//reduce
 // write a code to get sum of arr
 
 let sumOfArr = arr.reduce((acc, num) => {
-    console.log("acc: ", acc);
-    console.log("num: ", num);
-    return acc + num;
+  console.log("acc: ", acc);
+  console.log("num: ", num);
+  acc = acc + num;
+  return acc;
 });
-   
+
 console.log(sumOfArr);
 
+
+
+//write a code to calculate largest element in arr
+const ar = [-2, -5, -8, -6, -78]; 
+//output -> -2
+
+function largestElement(max, cur) {
+  if (cur > max) {
+    max = cur;
+  }
+  return max;
+}
+
+var ans = ar.reduce(largestElement,-Infinity);
+console.log(ans);
+
+
+var users = [
+  { firstName: "Mayank", lastName: "Singh", age: 55 },
+  { firstName: "Jyoti", lastName: "Jauhari", age: 25 },
+  { firstName: "Saket", lastName: "Bharti", age: 15 },
+];
+
+//write a code to get fullName of all the users
+// output-> ["Mayank Singh", "Jyoti Jauhari", "Saket Bharti"]
+
+function getFullName(obj) {
+  return obj.firstName + " " + obj.lastName;
+}
+
+var ans = users.map(getFullName)
+console.log(ans);
+
+console.log(users.map((obj)=> obj.firstName+" "+obj.lastName));
+
+
+
+//write a code to return the number of people with a particular age
+// output-> {55:1,25:1,15:1}
+//"bees in the knees"
+function peopleAge(robj, cobj) {
+  let age = cobj.age;
+  if (robj[age]) {
+    robj[age] = robj[age] + 1;
+  }
+  else {
+    robj[age] = 1;
+  }
+
+  return robj;
+
+}
+
+var obj = users.reduce(peopleAge, {}); 
+console.log(obj);
+
+
+//write a code to get firstName of all the users with age less than 30
+// output-> [ "Jyoti Jauhari", "Saket Bharti"]
+
+
+// hint : map and filter use hoga 
+var ans = users.filter((obj) => obj.age < 30)
+console.log(ans);
+var ans = ans.map(getFullName);
+console.log(ans);
+
+console.log(
+  users.filter((obj) => obj.age < 30)
+    .map(getFullName)
+);
+
+//using reduce
+function getPeopleLessThan30(arr, obj) {
+  if (obj.age < 30) {
+    arr.push(obj.firstName + " " + obj.lastName);
+  }
+  return arr;
+}
+
+var ans=users.reduce(getPeopleLessThan30,[]);
+console.log(ans);
 
 
 
