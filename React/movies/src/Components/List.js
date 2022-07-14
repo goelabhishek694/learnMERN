a01fa4b76ba01ad20dbaeabeab94f66cd8724a3f
goelabhishek694 <goelabhishek694@gmail.com>
2022-07-15 00:11:16 +0530

    :fire: axios,lifecycle method

--- React/movies/src/Components/List.js
+++ React/movies/src/Components/List.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
+// import { movies } from "./getMovies";
+import axios from 'axios'
+import API_KEY from "../secrets";
 export default class List extends Component {
   constructor() {
     super();
+    // console.log("constructor is called");
     this.state = {
       hover: "",
+      parr: [1], //ab tak main konse page par hu , or what page result am i showing ,
+      currPage: 1,
+      movies:[],
     };
   }
 
     handleEnter = (id) => {
         this.setState({
-          hover:id
+          hover: id
       })
   };
 
@@ -19,11 +25,24 @@ export default class List extends Component {
         hover: '',
       });
   };
+
+  async componentDidMount() {
+    // console.log("componentDidMount is called");
+    // console.log(API_KEY);
+    let ans = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}d&language=en-US&page=${this.state.currPage}`
+    );
+    // console.log(ans.data);
+    this.setState({
+      movies:[...ans.data.results] //[{},{},{}]
+    })
+  }
   render() {
-    let movie = movies.results; //fetch
+    // console.log("render is called");
+    // let movie = movies.results; //fetch
     return (
       <>
-        {movie.length == 0 ? (
+        {this.state.movies.length == 0 ? (
           <div className="spinner-grow text-success" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
@@ -33,7 +52,7 @@ export default class List extends Component {
               <strong>Trending</strong>
             </h3>
             <div className="movies-list">
-              {movie.map((movieObj) => (
+              {this.state.movies.map((movieObj) => (
                 <div
                   className="card movie-card"
                   onMouseEnter={() => this.handleEnter(movieObj.id)}
@@ -53,11 +72,11 @@ export default class List extends Component {
                         {movieObj.overview}
                       </p> */}
                   <div className="button-wrapper">
-                    {this.state.hover == movieObj.id && 
-                      <a href="#" class="btn btn-primary movie-button">
+                    {this.state.hover == movieObj.id && (
+                      <a href="#" class="btn btn-danger movie-button">
                         Add to Favourites
                       </a>
-                    }
+                    )}
                   </div>
                 </div>
               ))}
@@ -69,22 +88,16 @@ export default class List extends Component {
                     <a class="page-link" href="#">
                       Previous
                     </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      1
-                    </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      2
-                    </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      3
-                    </a>
-                  </li>
+                    </li>
+                    {
+                      this.state.parr.map(pageNum => (
+                        <li class="page-item">
+                          <a class="page-link" href="#">
+                            {pageNum}
+                          </a>
+                        </li>
+                      ))
+                    }
                   <li class="page-item">
                     <a class="page-link" href="#">
                       Next
