a5b8e8cc611fee23f4abe7e3496605f1142cc5c6
goelabhishek694 <goelabhishek694@gmail.com>
2022-07-19 23:02:30 +0530

    :construction: react-router-dom

--- React/movies/src/App.js
+++ React/movies/src/App.js
@@ -4,15 +4,17 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import List from './Components/List';
 import Favourites from './Components/Favourites';
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
   return (
-    <>
-      {/* <Navbar />
-      <Banner />
-      <List/> */}
-      <Navbar/>
-      <Favourites/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/fav" element={<Favourites />} />
+      </Routes>
+      {/* <Banner /> */}
+    </BrowserRouter>
   );
 }
 
--- React/movies/src/Components/Navbar.js
+++ React/movies/src/Components/Navbar.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-
+import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-        <div style={{display:'flex', background:"white", color:"blue",padding:'1rem', justifyContent:"center", alignItems:"center"}}>
-            <h1>Movies App</h1>
-            <h2 style={{marginLeft:"2rem",}}>Favourites</h2>
+      <div
+        style={{
+          display: "flex",
+          background: "white",
+          color: "blue",
+          padding: "1rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1>Movies App</h1>
+        </Link>
+
+        <Link to="/fav" style={{ textDecoration: "none" }}>
+          <h2 style={{ marginLeft: "2rem" }}>Favourites</h2>
+        </Link>
       </div>
-    )
+    );
   }
 }
