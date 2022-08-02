7b29e48c2781231d0b2b7c4e4cb3bbd40251a584
goelabhishek694 <goelabhishek694@gmail.com>
2022-08-02 22:32:38 +0530

    :tada: reels using nextJS

---
+++ React/reels/pages/category/fashion/clothes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>buying clothes </div>
+  )
+}
+
+export default index
---
+++ React/reels/pages/login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div>login page</div>
+  )
+}
+
+export default login
