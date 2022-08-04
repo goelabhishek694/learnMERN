9ba33d2def00ab869a844ca4bf8748d9c345a878
goelabhishek694 <goelabhishek694@gmail.com>
2022-08-04 23:47:16 +0530

    :lipstick: login UI completed

--- React/reels/pages/_app.js
+++ React/reels/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import './signup/signup.css'
 import './login/login.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- React/reels/pages/login/index.js
+++ React/reels/pages/login/index.js
@@ -3,46 +3,75 @@ import TextField from "@mui/material/TextField";
 import Image from "next/image";
 import logo from "../../assets/Instagram.jpeg";
 import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import IconButton from "@mui/material/IconButton";
+import { Carousel } from "react-responsive-carousel";
+import bg1 from '../../assets/bg1.jpg'
+import bg2 from '../../assets/bg2.jpg'
+import bg3 from '../../assets/bg3.jpg'
+import bg4 from '../../assets/bg4.jpg'
+import bg5 from '../../assets/bg5.jpg'
+
 
 function index() {
   return (
-    <div className="signup-container">
-      <div className="signup-card">
-        <Image src={logo} />
-        <TextField
-          id="outlined-basic"
-          size="small"
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-        />
-        <TextField
-          id="outlined-basic"
-          size="small"
-          label="Password"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-        />
-
-        <div style={{color:"blue", marginTop:"0.5rem"}}>Forget Password </div>
-
-        <Button
-          style={{ marginTop: "1rem" }}
-          variant="contained"
-          component="label"
-          fullWidth
-        >
-          Log in
-        </Button>
+    <div className="login-container">
+      <div className="insta-mob-bg">
+        <div className="carousel">
+          <Carousel
+            autoPlay
+            interval={2000}
+            infiniteLoop
+            showArrows={false}
+            showThumbs={false}
+            showIndicators={false}
+            stopOnHover
+            showStatus={false}
+          >
+            <Image src={bg1} />
+            <Image src={bg2} />
+            <Image src={bg3} />
+            <Image src={bg4} />
+            <Image src={bg5} />
+          </Carousel>
+        </div>
       </div>
-      <div className="bottom-card">
-        Don't Have an account ?{" "}
-        <span style={{ color: "blueviolet" }}>Signup</span>
+      <div>
+        <div className="login-card">
+          <Image src={logo} />
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="dense"
+          />
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="Password"
+            variant="outlined"
+            fullWidth
+            margin="dense"
+            type="password"
+          />
+
+          <div style={{ color: "blue", marginTop: "0.5rem" }}>
+            Forget Password{" "}
+          </div>
+
+          <Button
+            style={{ marginTop: "1rem" }}
+            variant="contained"
+            component="label"
+            fullWidth
+          >
+            Log in
+          </Button>
+        </div>
+        <div className="bottom-card">
+          Don't Have an account ?{" "}
+          <span style={{ color: "blueviolet" }}>Signup</span>
+        </div>
       </div>
     </div>
   );
