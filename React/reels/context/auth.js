d62e406a43b33390041ff100835218d8c06b1ab5
goelabhishek694 <goelabhishek694@gmail.com>
2022-08-09 23:55:42 +0530

    :fire: implemeted login

--- React/reels/context/auth.js
+++ React/reels/context/auth.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { auth} from "../firebase";
+export const AuthContext = React.createContext();
+import { signInWithEmailAndPassword } from "firebase/auth";
+function AuthWrapper({children}) {
+    console.log("in auth wrapper ");
+    //feature created 
+    function login(email,password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
-function AuthWrapper() {
-    console.log("hello in auth wrapper ")
-  return (
-    <div>hello</div>
-  )
+    const store = {
+        login
+    }
+
+
+    return (
+      <AuthContext.Provider value={store}>
+        {children}
+      </AuthContext.Provider>
+    );
 }
 
 export default AuthWrapper;
--- React/reels/pages/login/index.js
+++ React/reels/pages/login/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useContext} from "react";
 import TextField from "@mui/material/TextField";
 import Image from "next/image";
 import logo from "../../assets/Instagram.jpeg";
@@ -9,9 +9,37 @@ import bg2 from '../../assets/bg2.jpg'
 import bg3 from '../../assets/bg3.jpg'
 import bg4 from '../../assets/bg4.jpg'
 import bg5 from '../../assets/bg5.jpg'
-
+import { AuthContext } from '../../context/auth';
 
 function index() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const { login } = useContext(AuthContext);
+
+  let handleClick = async() => {
+    try {
+      console.log(email);
+      console.log(password);
+      setLoading(true);
+      setError('');
+      await login(email, password);
+      console.log("logged in");
+    }
+    catch (err) {
+      console.log("error ", JSON.stringify(err));
+      setError(err.code);
+      // use settimeout to remove error after 2sec
+      setTimeout(() => {
+        setError('');
+      }, 2000);
+    }
+    setLoading(false);
+    
+
+  }
   return (
     <div className="login-container">
       <div className="insta-mob-bg">
@@ -44,6 +72,8 @@ function index() {
             variant="outlined"
             fullWidth
             margin="dense"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <TextField
             id="outlined-basic"
@@ -53,8 +83,13 @@ function index() {
             fullWidth
             margin="dense"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-
+          {/* if error is present then show error */}
+          { error!="" &&
+            <div style={{ color: "red" }}>{ error }</div>
+          }
           <div style={{ color: "blue", marginTop: "0.5rem" }}>
             Forget Password{" "}
           </div>
@@ -64,6 +99,7 @@ function index() {
             variant="contained"
             component="label"
             fullWidth
+            onClick={handleClick}
           >
             Log in
           </Button>
