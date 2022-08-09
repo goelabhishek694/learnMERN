0ecbd5c6f5d9eb9051a3c8e13ed07ba7e1fae6b2
goelabhishek694 <goelabhishek694@gmail.com>
2022-08-09 21:52:19 +0530

    :lipstick: UI completed

--- React/reels/components/Feed.js
+++ React/reels/components/Feed.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Upload from "./Upload";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 function Feed() {
   return (
     <div className="feed-container">
@@ -8,7 +10,21 @@ function Feed() {
       <Upload />
       <div className="videos-container">
         <div className="post-container">
-          <video />
+          <video src="https://youtube.com/shorts/m2esthjzQmA" />
+          <div className="video-info">
+            <div className="avatar-container">
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/2.jpg"
+                sx={{ margin: "0.5rem" }}
+              />
+              <p>Abhishek Goel</p>
+            </div>
+            <div className="post-like">
+              <FavoriteIcon />
+              <p>10</p>
+            </div>
+          </div>
         </div>
         <div className="post-container">
           <video />
