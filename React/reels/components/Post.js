502b13182dc81f9965a74c33dde1ca644880c974
goelabhishek694 <goelabhishek694@gmail.com>
2022-09-12 23:46:26 +0530

    :lipstick: profile dynamic, comments modal

--- React/reels/components/Post.js
+++ React/reels/components/Post.js
@@ -2,12 +2,34 @@ import React, { useEffect, useState } from 'react'
 import Avatar from "@mui/material/Avatar";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
-import {db } from "../firebase";
+import { db } from "../firebase";
+import AddCommentIcon from "@mui/icons-material/AddComment";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
 function Post({ postData, userData}) {
     // console.log("123456",postData);
   const [like, setLike] = useState(false);
   // heart red -> jab logged in user ne like kia hta h 
+const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    console.log("dialog opened");
+  setOpen(true);
+};
+
+  const handleClose = () => {
+  console.log("dialog closed");
+  setOpen(false);
+};
   useEffect(() => {
     if (postData.likes.includes(userData.uid)) {
       setLike(true);
@@ -43,9 +65,82 @@ function Post({ postData, userData}) {
             />
             <p style={{ color: "white" }}>{postData.profileName}</p>
           </div>
-          <div className="post-like" style={like ? {color:"red"}: {color:"white"}}>
-            <FavoriteIcon onClick={handleLike} />
+          <div className="post-like">
+            <FavoriteIcon
+              style={like ? { color: "red" } : { color: "white" }}
+              onClick={handleLike}
+            />
             <p style={{ color: "white" }}>{postData.likes.length}</p>
+            <AddCommentIcon
+              onClick={handleClickOpen}
+              style={{ color: "white" }}
+            />
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+              fullWidth={true}
+              maxWidth="md"
+            >
+              <div className="modal-container">
+                <div className="video-modal">
+                  <video src={postData.postURL} />
+                </div>
+                <div className="comments-modal">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Lizards are a widespread group of squamate reptiles,
+                          with over 6,000 species, ranging across all continents
+                          except Antarctica
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        Share
+                      </Button>
+                    </CardActions>
+                  </Card>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Lizards are a widespread group of squamate reptiles,
+                          with over 6,000 species, ranging across all continents
+                          except Antarctica
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        Share
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </div>
+              </div>
+            </Dialog>
           </div>
         </div>
       </div>
--- React/reels/components/Profile.js
+++ React/reels/components/Profile.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Image from "next/image";
 import user from "../assets/avatar.png";
 import { AuthContext } from "../context/auth";
+
 import {
   collection,
   doc,
@@ -14,16 +15,29 @@ import { db } from "../firebase";
 function Profile() {
   const { user } = useContext(AuthContext);
   const [userData, setUserData] = useState({});
-  const [posts, setPosts] = useState([]);
+  const [postIds, setPostIds] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
   useEffect(() => {
     console.log(user.uid);
     const unsub = onSnapshot(doc(db, "users", user.uid), (doc) => {
       setUserData(doc.data());
+      setPostIds(doc.data().posts);
     });
     return () => {
       unsub();
     };
   }, [user]);
+
+  useEffect(() => {
+    let tempArr = [];
+    postIds.map(pid => {
+      const unsub = onSnapshot(doc(db, "posts", pid), (doc) => {
+        tempArr.push(doc.data());
+        setUserPosts([...tempArr]);
+        console.log("hello",tempArr);
+      });
+    })
+  }, [postIds]);
 
   // const myLoader = ({ src }) => {
   //   return `${userData.downloadURL}`;
@@ -36,7 +50,7 @@ function Profile() {
           <div
             style={{ height: "8rem", width: "8rem", clipPath: "circle(50%)" }}
           >
-            <Image layout="fill"  src={userData.downloadURL} />
+            <Image layout="fill" src={userData.downloadURL} />
           </div>
           <div>
             <h1>{userData.fullName}</h1>
@@ -44,14 +58,9 @@ function Profile() {
           </div>
         </div>
         <hr />
-        <div className="profile-posts">
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2Fa7cfed8e-8193-430d-909a-e10e64de47a8%2Fstream%20(2).mp4?alt=media&token=85920cf2-9f35-4bb6-8b12-f628d418e93b"></video>
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2Fa0cec983-0c29-48a8-bdcd-e54d5e577405%2Fa0ZOwXX_460svvp9.webm?alt=media&token=02f2d8fb-b77a-48b9-827c-fadcfe9b32d3"></video>
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-          <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-        </div>
+        <div className="profile-posts">{userPosts.map(post => (
+          <video src={post.postURL}></video>
+        ))}</div>
       </div>
     </div>
   );
